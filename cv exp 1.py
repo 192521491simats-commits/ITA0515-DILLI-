7371d3f59071b3fa577f9912480197b3b599f615
192521491simats-commits <192521491.simats@saveetha.com>
2026-08-12 10:37:57 +0530

    Add files via upload

---
+++ cv exp 1.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread("image.jpg.png")
+
+if img is None:
+    print("Error: Image not found!")
+    exit()
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Original Image", img)
+cv2.imshow("Grayscale Image", gray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
